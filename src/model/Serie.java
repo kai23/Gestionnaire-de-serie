d21172cafdb11394f65d219cf2dc67c329d8d2b9
cactusmutant <cactus@localhost.localdomain>
2012-09-17 12:42:37 +0200

    premier constructeur pour Serie

--- src/model/Serie.java
+++ src/model/Serie.java
@@ -3,6 +3,11 @@ package model;
 import java.util.ArrayList;
 
 public class Serie {
+	public Serie(int id, String name = "") {
+		this.id = id;
+		this.name = name;
+	}
+	
 	private int id;
 	private String name;
 	private ArrayList<Season> seasons;
