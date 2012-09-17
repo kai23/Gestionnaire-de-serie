0a3e900c78750e361f926c65beddf56cd33494b1
Florian <kai_23_@hotmail.com>
2012-09-17 19:32:29 +0200

    update main et de la fonction de getFile

--- src/application/Main.java
+++ src/application/Main.java
@@ -17,10 +17,18 @@ public class Main {
 		 */
 		
 		
+		// Le chemin qui est à récupérer via JFileChooser
 		String chemin = "/home/";
+		
+		// Notre controleur de série
 		SerieController controller = new SerieController();
+		
+		// On récupère la liste des fichiers du dossier
 		ArrayList<String> listeFichiers = controller.listDirectory(chemin);
-	
+		
+		// On affiche notre liste
+		System.out.println(listeFichiers);
+		
 	}
 
 }
