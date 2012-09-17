1ea33c6a3b1b07f045e6dbb869144c4afc4d1964
sarah <sarahjayneburrell11@gmail.com>
2012-09-17 14:45:40 +0200

    merge

--- src/controlleur/SerieController.java
+++ src/controlleur/SerieController.java
@@ -5,11 +5,8 @@ public class SerieController {
 	/**
 	 * @param args
 	 */
-<<<<<<< HEAD
-	public static void main (String[] args) {	
-=======
+
 	public static void main(String[] args) {	
->>>>>>> branch 'master' of https://github.com/kai23/Gestionnaire-de-serie.git
 		
 	}
 
