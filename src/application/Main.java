a2af0be593d3c863d86b0cab3b8b9019dcf010c4
Florian <kai_23_@hotmail.com>
2012-09-17 17:51:04 +0200

    Ajout de la classe main !

---
+++ src/application/Main.java
@@ -0,0 +1,17 @@
+package application;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/*
+		 * 
+		 * Faire tous les tests ici !
+		 * 
+		 */
+
+	}
+
+}
--- src/controlleur/SerieController.java
+++ src/controlleur/SerieController.java
@@ -1,13 +1,20 @@
 package controlleur;
 
+import java.io.File;
+
 public class SerieController {
 
 	/**
 	 * @param args
 	 */
 
-	public static void main(String[] args) {	
+	private void getListeEpisodeDossier() {
+		File repertoire = new File("/");
+		File[] files=repertoire.listFiles();
 		
+		for (File file : files) {
+			System.out.println(file.toString());
+		}
 	}
 
 }
