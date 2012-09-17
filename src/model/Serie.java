0bf4a66e2f9b898375e6422c9133ff7884e8acd3
Florian <kai_23_@hotmail.com>
2012-09-17 14:46:52 +0200

    changement de Série

--- src/model/Serie.java
+++ src/model/Serie.java
@@ -2,15 +2,23 @@ package model;
 
 import java.util.ArrayList;
 
+
+
+
+
 public class Serie {
-	public Serie(int id, String name = "") {
-		this.id = id;
-		this.name = name;
-	}
 	
 	private int id;
 	private String name;
 	private ArrayList<Season> seasons;
 	private Episode currentEpisode;
 	private String description;
+	
+	
+	public Serie(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	
 }
