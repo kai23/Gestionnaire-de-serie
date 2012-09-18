aff4bd2b4b8acbadb1f7d141547920ad98bfd4c4
cactusmutant <cactus@localhost.localdomain>
2012-09-18 09:49:10 +0200

    Ajout des panels et de la toolbar dans Window

--- src/view/Window.java
+++ src/view/Window.java
@@ -1,14 +1,24 @@
 package view;
 
+import javax.swing.JToolBar;
+
 import java.awt.BorderLayout;
+
+import java.awt.Container;
 
 import javax.swing.JFrame;
 
 public class Window extends JFrame {
+	private JToolBar toolbar;
+	
 	public Window() {
 		super("Ma fenêtre");
 
 		setLayout(new BorderLayout());
-		add(BorderLayout.CENTER);
+		Container pane = getContentPane();
+
+		pane.add(toolbar, BorderLayout.PAGE_START);
+		pane.add(new PanelSerie(), BorderLayout.LINE_START);
+		pane.add(new PanelMain(), BorderLayout.CENTER);
 	}
 }
