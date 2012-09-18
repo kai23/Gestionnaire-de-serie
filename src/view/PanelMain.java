1cc224339868bd00528511ce3a665a55035614e8
sburrell <sarah.burrell@ingesup.com>
2012-09-18 12:11:42 +0200

    Modif panel main

--- src/view/PanelMain.java
+++ src/view/PanelMain.java
@@ -1,12 +1,15 @@
 package view;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.TextArea;
 
+import javax.swing.BoxLayout;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.ScrollPaneLayout;
 
 public class PanelMain extends JPanel {
     public PanelMain() {
@@ -17,9 +20,16 @@ public class PanelMain extends JPanel {
     	//JLabel infoBox = new JLabel(serieCtrl.getInfo(););
     	
     	
+    	//Creation panel saisons
+    	JPanel panelSeasons = new JPanel();
+    	panelSeasons.setLayout(new ScrollPaneLayout());
+    	panelSeasons.setBackground(Color.cyan);
+    	
     	//Ajout des composants
     	add(infoBox, BorderLayout.PAGE_START);
-    	add(new JComboBox());
+    	add(panelSeasons, BorderLayout.CENTER);
+    	
+    	
     	
     	
 		
