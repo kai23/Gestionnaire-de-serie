7fefe2cdd37b5483fcc92c489729058f14c359ec
cactusmutant <cactus@localhost.localdomain>
2012-09-19 12:33:37 +0200

    Suppression d'un println un peu trop verbeux

--- src/view/PanelSerie.java
+++ src/view/PanelSerie.java
@@ -15,6 +15,7 @@ import controlleur.SerieController;
 public class PanelSerie extends JPanel implements ListSelectionListener{
 	private static final long serialVersionUID = 1L;
 	private PanelMain infoSerie;
+	private DefaultListModel<String> listModel;
 
 	public PanelSerie(PanelMain infoSerie){
 		super();
@@ -31,15 +32,16 @@ public class PanelSerie extends JPanel implements ListSelectionListener{
 		mesSeries.add("TBBT");
 		mesSeries.add("HIMYM");
 		mesSeries.add("The Simpsons");
+		mesSeries.add("Ça c'est de l'erreur qui tue et qui a un nom a dormir debout");
 			
 		//Passage par un defaultListModel
-		DefaultListModel<String> listModel = new DefaultListModel<>();
-		for(int i = 0; i <mesSeries.size(); i++){
-			listModel.addElement(mesSeries.get(i));
+		listModel = new DefaultListModel<>();
+		JList listSerie = new JList(listModel);
+		for(String maSerie : mesSeries) {
+			addSerie(maSerie);
 		}
 		
 		//Creation et remplissage de la JList
-		JList listSerie = new JList(listModel);
 		listSerie.addListSelectionListener(this);
 		
 		//Ajout des components
@@ -49,14 +51,16 @@ public class PanelSerie extends JPanel implements ListSelectionListener{
 
 	@Override
 	public void valueChanged(ListSelectionEvent e) {
-		System.err.println("Source : "+ e.getSource());
 		//Recuperer le nom de la serie choisie
 		String nameSerie = (String) (((JList) e.getSource()).getSelectedValue());
 		System.out.println(nameSerie);
 
 		infoSerie.setSerieName(nameSerie);
-//		infoSerie.updateUI();
 		infoSerie.updateView();
 
 		}
+
+	private void addSerie(String name) {
+		listModel.addElement(name);
+	}
 }
