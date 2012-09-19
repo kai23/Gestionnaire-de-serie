141a2b663e8f825a1e689ba8b398534fd974d326
Florian <kai_23_@hotmail.com>
2012-09-19 17:38:13 +0200

    Ajout de la recherche !

--- src/view/AddSerieFrame.java
+++ src/view/AddSerieFrame.java
@@ -3,39 +3,52 @@ import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.util.List;
+import java.util.Vector;
 
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTable;
 import javax.swing.JTextField;
 
 import com.moviejukebox.thetvdb.model.Series;
 
 import controlleur.SerieController;
 import javax.swing.JSeparator;
+import javax.swing.table.DefaultTableModel;
+
+import java.awt.Window.Type;
+import java.util.Locale;
 
 class AddSerieFrame extends JFrame {
 	private SerieController controller = new SerieController();
 	private JTextField textField;
-	
+	private String nomSerie;
 	
 	public AddSerieFrame(){
+		setSize(new Dimension(200, 500));
+		setLocale(Locale.FRANCE);
+		setTitle("Ajouter une série");
 		getContentPane().setLayout(null);
 		setVisible(true);
 		setSize(500, 200);
 		
 		
 		JLabel lblEntrerLeNom = new JLabel("Entrer le nom de la série");
-		lblEntrerLeNom.setBounds(61, 25, 131, 14);
+		lblEntrerLeNom.setBounds(10, 25, 182, 14);
 		getContentPane().add(lblEntrerLeNom);
 		
 		textField = new JTextField();
+		textField.setToolTipText("Le nom de la série");
 		textField.setBounds(202, 22, 86, 20);
 		getContentPane().add(textField);
 		textField.setColumns(10);
@@ -58,5 +71,40 @@ class AddSerieFrame extends JFrame {
 		btnParcourirUnDossier.setBounds(148, 127, 152, 23);
 		getContentPane().add(btnParcourirUnDossier);
 		
+		JButton btnGo = new JButton("Go");
+		btnGo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				nomSerie = textField.getText();
+				List<Series> series = controller.trouverSerieTVDB(nomSerie);
+				JDialog boiteDialogue = new JDialog();
+				
+				
+				boiteDialogue.setVisible(true);
+				boiteDialogue.setSize(new Dimension(200,300));
+				JTable maTable = new JTable(new DefaultTableModel(series.size(), 1));
+				DefaultTableModel model = (DefaultTableModel) maTable.getModel();
+				for (Series series2 : series) {
+					model.addRow(new String[]{series2.getSeriesName()});
+				}
+				boiteDialogue.add(maTable);
+				maTable.setVisible(true);
+				
+				
+				
+				
+			}
+		});
+		btnGo.setBounds(300, 21, 58, 23);
+		getContentPane().add(btnGo);
+		
+	}
+
+	public String getNomSerie() {
+		return nomSerie;
+	}
+
+	public void setNomSerie(String nomSerie) {
+		this.nomSerie = nomSerie;
 	}
 }
