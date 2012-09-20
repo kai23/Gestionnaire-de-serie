ab5abfcc5d1dd3d56652be1821f7a1a46ae3c2c1
sburrell <sarah.burrell@ingesup.com>
2012-09-20 11:35:06 +0200

    Affichage des JTables dans la panel principale

--- src/view/PanelMain.java
+++ src/view/PanelMain.java
@@ -16,6 +16,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 import controlleur.SerieController;
 
@@ -27,90 +28,121 @@ public class PanelMain extends JPanel implements ActionListener {
 	
 	/* ATTRIBUTS */
 	//Widgets
-	private JLabel wInfoBox = new JLabel("Les Mutants");
+	private JLabel wInfoBox = new JLabel();
+	private ArrayList<SeasonField> wSeasons;
+	private JPanel panelScrollpane;
 	public class SeasonField {
-		public SeasonField(JRadioButton title, JList episodes) {
+		public JRadioButton title;
+		public JTable episodes;
+		public SeasonField(JRadioButton title, JTable episodes) {
 			this.title = title;
 			this.episodes = episodes;
 		}
-		public JRadioButton title;
-		public JList episodes;
 	}
+	
 	private Serie serie;
-	private ArrayList<SeasonField> wSeasons;
-	private JPanel panelScrollpane;
 	private SerieController ctrl;
 
+	/**
+	 * Constructor which creates the panel of seasons and episodes
+	 * @param ctrl
+	 */
     public PanelMain(SerieController ctrl) {
     	super();
+    	//Initialisation du controlleur
     	this.ctrl = ctrl;
-    	setLayout(new BorderLayout());
+    	//Initialisation de la série
     	setSerie(getSerie());
+    	
+    	/***** AFFICHAGE *****/
+    	setLayout(new BorderLayout());
 		wSeasons = new ArrayList<SeasonField>();
-
-    	//Creation des scrollbar
-		panelScrollpane = new JPanel();
+		panelScrollpane = new JPanel(); //Creation de la scrollbar
 		JScrollPane scrollpane = new JScrollPane(panelScrollpane);
-
-		panelScrollpane.setLayout(new BoxLayout(panelScrollpane, BoxLayout.PAGE_AXIS));
-				
+		panelScrollpane.setLayout(new BoxLayout(panelScrollpane, BoxLayout.PAGE_AXIS));		
     	add(wInfoBox, BorderLayout.PAGE_START);
     	add(scrollpane, BorderLayout.CENTER);
 	}
 
     /* GETTEURS ET SETTEURS */
+    /**
+     * Serie getter
+     * @return serie
+     */
 	public Serie getSerie() {
 		return serie;
 	}
-
+	/**
+	 * Serie setter
+	 * @param serie
+	 */
 	public void setSerie(Serie serie) {
 		this.serie = serie;
 	}
 
+	/**
+	 * Update of the panel when change
+	 */
 	public void updateView() {
 		this.wInfoBox.setText(this.serie.getName());
 		panelScrollpane.removeAll();
-		wSeasons.clear();
-
+		wSeasons.clear();	
+		
 		for (Season season : serie.getAllSeason()) {
 			JPanel panelSeason = new JPanel(new BorderLayout());
-			panelScrollpane.add(panelSeason);
-			
+			panelScrollpane.add(panelSeason);			
 			JRadioButton seasonLabel = new JRadioButton("Saison " + season.getNum());
 			seasonLabel.setIcon(new Icon() {
 					public void paintIcon(Component c, Graphics g, int x, int y) {}
 					public int getIconWidth() { return 0; }
 					public int getIconHeight() { return 0; }
 				});
-			seasonLabel.addActionListener(this);
-		
+			seasonLabel.addActionListener(this);		
 			panelSeason.add(seasonLabel, BorderLayout.PAGE_START);
 	
-			DefaultListModel<String> listModel = new DefaultListModel<String>();
+			//Recuperation des episodes
+			/*DefaultListModel<String> listModel = new DefaultListModel<String>();
 			for (Episode episode : season.getAllEpisodes()) {
 				listModel.addElement(episode.getName());
 			}
-			JList listEpisodes = new JList(listModel);
-
-			panelSeason.add(listEpisodes, BorderLayout.CENTER);
-			wSeasons.add(new SeasonField(seasonLabel, listEpisodes));
+			JList listEpisodes = new JList(listModel);*/
+			
+			//Composition de la table
+			String[] columnNames = {"N°", "Title", "Length", "Seen"};
+			Object[][] data = new Object[season.getAllEpisodes().size()][2];
+			//Pour chaque episode
+			int i = 0; 
+			for(Episode episode : season.getAllEpisodes()){
+				data[i][0] = episode.getNum();
+				data[i][1] = episode.getName();
+			//	data[i][2] = episodes.getLength();
+			//	data[i][3] = episodes.isWatched();				
+				i++;
+			}
+			wSeasons.add(new SeasonField(seasonLabel, new JTable(data, columnNames)));
 		}
 	
 		this.updateUI();
 	}
 	
+	/**
+	 * Listener
+	 * @param : actionEvent e
+	 */
 	public void actionPerformed(ActionEvent e) {
 		for (SeasonField season : wSeasons) {
-			if (e.getSource() == season.title)
+			if (e.getSource() == season.title){
 				season.episodes.setVisible(!season.episodes.isVisible());
+				System.out.println(season.episodes);
+			}
 		}
 	}
 
-	public void showInfoSerie(String serieName) {
-		JOptionPane.showMessageDialog(null, serieName, "test", JOptionPane.INFORMATION_MESSAGE);		
-	}
-
-
+	
+	/**
+	 * Function allowing the transfer of informations from one panel to the other
+	 * @param nameSerie
+	 */
 	public void transferInfo(String nameSerie) {
 		setSerie(ctrl.getSerieByName(nameSerie));
 	}
