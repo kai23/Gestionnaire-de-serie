b5f8f751da8e118146cfabf4fb302a843db02f4b
sburrell <sarah.burrell@ingesup.com>
2012-09-20 12:20:51 +0200

    Avancement sur la base maCollection

--- src/model/MaCollection.java
+++ src/model/MaCollection.java
@@ -1,9 +1,69 @@
-package Model;
+package model;
+
+import java.util.ArrayList;
 
 public class MaCollection {
+	//Attributs
 	private ArrayList<Serie> series;
-
+	
+	/**
+	 * Constructeur par défaut
+	 */
 	public MaCollection() {
-		series = new ArrayList<Serie>();
+		series = loadSeries();
 	}
+
+	/**
+	 * Getter of series
+	 * @return ArrayList
+	 */
+	public ArrayList<Serie> getSeries() {
+		return series;
+	}
+
+	/**
+	 * Setter of series
+	 * @param series
+	 */
+	public void setSeries(ArrayList<Serie> series) {
+		this.series = series;
+	}
+	
+	/**
+	 * Fonction permettant de recuperer une série à partir de son identifiant
+	 * @param id
+	 * @return serie || null
+	 */
+	public Serie getSerieById(String id){
+		for(int i = 0; i <series.size(); i++){
+			if(series.get(i).getId().equals(id))
+				return series.get(i);
+		}
+		return null;
+	}
+	
+	/**
+	 * Fonction permettant de recuperer une série à partir de son titre
+	 * @param name
+	 * @return serie || null
+	 */
+	public Serie getSerieByName(String name){
+		for(int i = 0; i <series.size(); i++){
+			if(series.get(i).getName().equals(name))
+				return series.get(i);
+		}
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public ArrayList<Serie> loadSeries() {
+		int i = 0;
+		series.get(i).
+		return null;
+	}
+	
+	
 }
